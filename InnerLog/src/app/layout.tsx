d501b7592dfffa4f1547b240240431cb40b0b090
Hubott <hugowynnthomas@gmail.com>
2026-02-15 10:39:06 +1100

    fix: Added safe-top class to layout.tsx wrapping whole app

--- InnerLog/src/app/layout.tsx
+++ InnerLog/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>
+      <body className="safe-top">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
