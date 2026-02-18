5789e8eba0a0a606931cbb91dd23a3c07e7ca2b8
Hubott <hugowynnthomas@gmail.com>
2026-02-18 11:13:05 +1100

    feat: Added loading screens to pages

---
+++ InnerLog/src/app/home/loading.tsx
@@ -0,0 +1,20 @@
+import { HomeButton } from "../_components/home";
+
+export default function Loading() {
+  return (
+    <div className="p-6 space-y-6 animate-pulse">
+      {/* Title */}
+      <div className="h-8 w-48 bg-gray-300 rounded" />
+
+      {/* Card */}
+      <div className="space-y-3">
+        <div className="h-4 w-full bg-gray-300 rounded" />
+        <div className="h-4 w-5/6 bg-gray-300 rounded" />
+        <div className="h-4 w-2/3 bg-gray-300 rounded" />
+      </div>
+
+      {/* Big box */}
+      <div className="h-40 w-full bg-gray-300 rounded-xl" />
+    </div>
+  );
+}
